d90b31d157f6d77e2c11ef300b480e4d4d443942
Gaurav <gaurav.sumra@warbyparker.com>
2018-12-02 14:02:04 -0500

    Calculate Extended Kalman filter using Radar

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -155,10 +155,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // Radar updates
-  } else {
-    // Laser updates
 
+    // Radar updates
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+
+  } else {
+
+    // Laser updates
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
     ekf_.update(measurement_pack.raw_measurements_);
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -55,4 +55,49 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  if (fabs(py) < 0.0001) {
+    py = 0.0001;
+  }
+  if(fabs(px) < 0.0001) {
+    px = 0.0001;
+  }
+
+  float rho = sqrt(px*px + py*py);
+  if(fabs(rho) < 0.0001){
+    rho = 0.0001;
+  }
+
+  float theta = atan2(py, px);
+  float pv = px*vx + py*vy;
+  float rho_dot = pv / rho;
+
+  VectorXd z_pred(3);
+  z_pred << rho, theta, rho_dot;
+
+  VectorXd y = z - z_pred;
+
+  // Make theta between -PI and PI
+  while (y(1) > M_PI) {
+     y(1) -= 2 * M_PI;
+  }
+  while (y(1) < -M_PI) {
+     y(1) += 2 * M_PI;
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
